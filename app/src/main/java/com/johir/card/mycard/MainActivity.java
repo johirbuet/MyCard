3acc6424503656e27da4852f8f8caf722b0b35c9
Johirul Islam <johirbuet@gmail.com>
2017-07-01 19:55:23 -0500

    Small changes

--- app/src/main/java/com/johir/card/mycard/MainActivity.java
+++ app/src/main/java/com/johir/card/mycard/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
